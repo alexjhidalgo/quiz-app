57b1b50ec418c13b905dd36abeb00c29e15a88ca
aalexmann <alex.h@Alexs-MacBook.local>
2019-10-17 17:41:05 -0400

    Achieved app functionality

---
+++ second.js
@@ -0,0 +1,205 @@
+function renderHeader(){
+    $(document).ready(function(){
+        $('.header').html(`
+        Score:
+        ${STORE.answersCorrect}/${STORE.qPages.length}
+        <br>
+        `)
+    })
+}
+
+function updateHeaderQNumber(){
+
+    STORE.i++;
+    $('p').detach()
+    $('.header').prepend(`
+    <p> Question: ${STORE.i}/${STORE.qPages.length} 
+    </p>`)
+
+}
+
+function updateHeaderCorrect(){
+
+        STORE.currentQuestion++
+        STORE.answersCorrect++
+
+        $('.header').html(`
+
+        Score:
+        ${STORE.answersCorrect}/${STORE.qPages.length}
+        `)
+        console.log(`updateHeader Ran`)
+
+}
+
+function updateHeaderIncorrect(){
+
+    STORE.currentQuestion++
+
+    $('.header').html(`
+
+    Score:
+    ${STORE.answersCorrect}/${STORE.qPages.length}
+    `)
+    console.log(`updateHeader Ran`)
+
+}
+
+
+// function renderLandingPage(){
+//     $(document).ready(function(){
+//         $('.landingPage').html(`
+//         <h1>Like quoting movies and TV Shows?</h1>
+//         <h2>Take a quiz to see what you can quote!</h2>
+//         <button> Start Quiz! </button>
+//         `)
+//     })
+// }
+
+function hidelandingpage(){
+    $('.landingPage').on('click', 'button', function(){
+        $('.landingPage').hide();
+        $('.questionsPage').show();
+        updateHeaderQNumber();
+        $('.header').show();
+    })
+}
+
+
+// function renderNextQuestionsPage(){
+//     $('.answersPage').on('click', 'button', function(){
+
+        
+//         $('.answersPage').hide();
+//         $('.questionsPage').show();
+//     })
+// }
+
+function renderQuestionsPage(){
+    $('.js-qRender').on('click', 'button', function(){
+
+        $('.answersPage').hide();
+
+        if (STORE.currentQuestion < 10){
+            $('.questionsPage').show();
+        }
+        
+
+        const qPageNum = STORE.qPages[STORE.currentQuestion]
+
+        $('.questionsPage').html(`
+        ${STORE.qPages[STORE.currentQuestion].startQuote}
+        <br> 
+        <h3>What comes next?</h3>
+        <form>
+            <input value='0' id='ans0' type='radio' name='endQuote' checked>
+                <label>${qPageNum.answers[0]}</label>
+   
+            <br>
+
+            <input value='1' id='ans1' type='radio' name='endQuote'>
+                <label>${qPageNum.answers[1]}</label>
+
+            <br>
+
+            <input value='2'id='ans2' type='radio' name='endQuote'>
+                <label>${qPageNum.answers[2]}</label>
+ 
+            <br>
+
+            <input value='3' id='ans3' type='radio' name='endQuote'>
+                <label>${qPageNum.answers[3]}</label>
+        </form>
+
+        <button> Final Answer </button>
+        `)
+    })
+}
+
+
+
+function renderAnswersPage(){
+    $('.questionsPage').on('click', 'button', function(){
+        $('.questionsPage').hide();
+        $('.landingPage').hide();
+
+        let chosenElem = document.querySelector('input[name=endQuote]:checked ~ label');
+        let chosenAnswer = chosenElem.innerHTML;
+        let i = STORE.currentQuestion;
+        let correctAnswer = STORE.qPages[i].theEndQuote;
+        
+
+        if (chosenAnswer === correctAnswer){
+
+            $('.answersPage').html(`
+            <h1> That\'s Right! <h1>
+            <h2> The full quote is "${STORE.qPages[i].startQuote} ... ${STORE.qPages[i].theEndQuote}" </h2>
+            <button> Next </button>
+            `)
+            updateHeaderCorrect();
+            updateHeaderQNumber();
+        }
+
+        else if (chosenAnswer !== correctAnswer){
+
+            $('.answersPage').html(`
+            <h1> That\'s not Right! <h1>
+            <h2> The full quote is "${STORE.qPages[i].startQuote} ... ${STORE.qPages[i].theEndQuote}" </h2>
+            <button> Next </button>
+            `)
+            updateHeaderIncorrect();
+            updateHeaderQNumber();
+        }
+
+
+
+
+        $('.answersPage').show();
+    })
+
+}
+
+function renderSummaryPage(){
+    $('.answersPage').on('click', 'button', function(){
+        $('.summaryPage').html(`
+        <h1> Way to go! </h1>
+        <h2> You completed the quiz!</h2>
+        <h2> and your score is ${STORE.answersCorrect} out of 10!</h2>
+        <button> Try Again? </button>
+        `)
+        if(STORE.currentQuestion === 10){
+            $('.header').hide();
+            $('.questionsPage').hide();
+            $('.summaryPage').show();
+        }
+    })
+}
+
+function showLandingPageAgain(){
+    $('.summaryPage').on('click', 'button', function(){
+        $('.summaryPage').hide();
+        renderHeader();
+        $('.landingPage').show();
+        STORE.currentQuestion = 0;
+        STORE.score = 0;
+        STORE.i = 0;
+        STORE.answersCorrect = 0;
+
+    })
+}
+
+
+function initialize(){
+    $('.questionsPage').hide();
+    $('.answersPage').hide();
+    $('.summaryPage').hide();
+    renderHeader();
+    hidelandingpage();
+    renderQuestionsPage();
+    renderAnswersPage();
+    renderSummaryPage();
+    showLandingPageAgain();
+    console.log(`initialize function ran`)
+}
+
+$(initialize)
---
+++ store.js
@@ -0,0 +1,106 @@
+const STORE = {
+    qPages: [{
+        startQuote:  'Question, which bear is best?',
+        answers: [
+            'That\'s a ridiculous question.',
+            'Easy, a polar bear. You know why?',
+            'The kind that ride unicycles.',
+            'Uhm the ones that prevent forest fires?'
+        ],
+        theEndQuote: 'That\'s a ridiculous question.'
+    },
+    {
+        startQuote:  'And you never say thank you...',
+        answers: [
+            'Well you never said sorry.',
+            'And that means so much to you?',
+            'Thank you. Are you happy now?',
+            'That\'s what the money\'s for!'
+        ],
+        theEndQuote: 'That\'s what the money\'s for!'
+    },
+    {
+        startQuote:  'It\'s Friday, you ain\'t got no job...',
+        answers: [
+            'Ain\'t got no \'sponsabilities neither.',
+            'I got you covered though.',
+            'So im\'ma get you high today.',
+            'And you ain\'t got S#%T to do.'
+        ],
+        theEndQuote: 'And you ain\'t got S#%T to do.'
+    },
+    {
+        startQuote:  'Your voice is like a combination of Fergie...',
+        answers: [
+            'and P!nk.',
+            'and Jesus.',
+            'and Britney Spears.',
+            'and Euterpe.'
+        ],
+        theEndQuote: 'and Jesus.'
+    },
+    {
+        startQuote:  'That was such a high...',
+        answers: [
+            'I can\'t wait to do it again.',
+            'Almost makes me want to do it again.',
+            'I don\'t know why anybody does drugs.',
+            'But it can never happen again. I mean it.'
+        ],
+        theEndQuote: 'I don\'t know why anybody does drugs.'
+    },
+    {
+        startQuote:  'What about the reality we left behind?',
+        answers: [
+            'Who cares? We were miserable anyway.',
+            'What about the reality where Hitler cured cancer? Just don\'t think about it.',
+            'It doesn\'t matter now; there\'s nothing we can do about.',
+            'Look i\'m not going over this again with you; just let things go.'
+        ],
+        theEndQuote: 'What about the reality where Hitler cured cancer? Just don\'t think about it.'
+    },
+    {
+        startQuote:  'You stay classy San Diego...',
+        answers: [
+            'And thanks for stopping by.',
+            'I know I will.',
+            'Come back soon.',
+            'And I will too hahaha.'
+        ],
+        theEndQuote: 'And thanks for stopping by.'
+    },
+    {
+        startQuote:  '\'You\'re like REALLY pretty.\'\'Thanks.\'... ',
+        answers: [
+            'Do you think I could be a model?',
+            'She\'s taken.',
+            'You know what, I take it back.',
+            'So you agree? What? You think you\'re really pretty?'
+        ],
+        theEndQuote: 'So you agree? What? You think you\'re really pretty?'
+    },
+    {
+        startQuote:  'Here\'s looking at you...',
+        answers: [
+            'And here\'s looking at you.',
+            'Kid.',
+            'Babe.',
+            'And never looking back.'
+        ],
+        theEndQuote: 'Kid.'
+    },
+    {
+        startQuote:  'Nobody puts baby...',
+        answers: [
+            'On a corner!',
+            'Against a wooden fence!',
+            'In a corner!',
+            'Against a steel fence!'
+        ],
+        theEndQuote: 'In a corner!'
+    }],
+    currentQuestion: 0,
+    score: 0,
+    i: 0,
+    answersCorrect: 0,
+}
